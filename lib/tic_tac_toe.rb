fd0f6e2dd92531eeb1318345a56da1ea9ab8ead5
Scott Weinstein <sweinstein27@gmail.com>
2018-04-19 21:05:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,13 +38,14 @@ end
 def play(board)
   until over?(board)
     turn(board)
+  end
     if won?(board)
     puts "Congratulations #{winner(board)}"
   elsif draw?(board)
     puts "Cat's Game!"
   end
 end
-end
+
 
 
 
