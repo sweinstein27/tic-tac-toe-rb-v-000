b226485c26e577a47a36db97b18fd9395e8bbad0
Scott Weinstein <sweinstein27@gmail.com>
2018-04-19 20:48:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,7 +62,10 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
+  if turn_count(board).even?(board)
+    "X"
+  else "O"
+end
 end
 
 def position_taken?(board, index)
